93f09b7d0c3248b6aa44c81fbf3d766c9fb36faa
William Lebel <lebelwilliam@gmail.com>
2013-05-12 14:38:18 -0400

    Fixed color + header

--- src/main/java/com/greatmancode/mmoInfoFactionTerritory/mmoInfoFactionTerritory.java
+++ src/main/java/com/greatmancode/mmoInfoFactionTerritory/mmoInfoFactionTerritory.java
@@ -14,41 +14,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-/*
- * This file is part of mmoInfoFactionName <http://github.com/greatman/mmoInfoFactionName>.
- *
- * mmoInfoFactionName is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-/*
- * This file is part of mmoInfoHealth <http://github.com/mmoMinecraftDev/mmoInfoFood>.
- *
- * mmoInfoHealth is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package com.greatmancode.mmoInfoFactionTerritory;
 
-import java.awt.Color;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -109,7 +76,7 @@ public final class mmoInfoFactionTerritory extends MMOPlugin
 				Player player = getScreen().getPlayer();
 				FPlayer fPlayer = FPlayers.i.get(player);
 				Faction faction = Board.getFactionAt(new FLocation(player.getLocation()));
-				setText(String.format(ChatColor.AQUA + "Location" + Color.WHITE + ": "+ fPlayer.getColorTo(faction) + faction.getTag(), new Object[0]));
+				setText(String.format(ChatColor.AQUA + "Location" + ChatColor.WHITE + ": "+ fPlayer.getColorTo(faction) + faction.getTag(), new Object[0]));
 			}
 		}
 	}
